72bbfd979459e88254128673013c9a0d14444868
madsex <145916188+daosanchez@users.noreply.github.com>
2024-01-02 13:21:39 +0300

    Add files via upload

---
+++ datetime-kullanimi3/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace datetime_kullanimi3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMesaj = new System.Windows.Forms.Label();
+            this.btnDurdur = new System.Windows.Forms.Button();
+            this.btnKur = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dtpZaman = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Font = new System.Drawing.Font("Javanese Text", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ALARM ZAMANI:";
+            // 
+            // lblMesaj
+            // 
+            this.lblMesaj.AutoSize = true;
+            this.lblMesaj.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblMesaj.Font = new System.Drawing.Font("Javanese Text", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMesaj.Location = new System.Drawing.Point(21, 195);
+            this.lblMesaj.Name = "lblMesaj";
+            this.lblMesaj.Size = new System.Drawing.Size(39, 36);
+            this.lblMesaj.TabIndex = 1;
+            this.lblMesaj.Text = "....";
+            // 
+            // btnDurdur
+            // 
+            this.btnDurdur.Font = new System.Drawing.Font("Javanese Text", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDurdur.Location = new System.Drawing.Point(12, 127);
+            this.btnDurdur.Name = "btnDurdur";
+            this.btnDurdur.Size = new System.Drawing.Size(136, 44);
+            this.btnDurdur.TabIndex = 2;
+            this.btnDurdur.Text = "DURDUR";
+            this.btnDurdur.UseVisualStyleBackColor = true;
+            this.btnDurdur.Click += new System.EventHandler(this.btnDurdur_Click);
+            // 
+            // btnKur
+            // 
+            this.btnKur.Font = new System.Drawing.Font("Javanese Text", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKur.Location = new System.Drawing.Point(12, 66);
+            this.btnKur.Name = "btnKur";
+            this.btnKur.Size = new System.Drawing.Size(136, 44);
+            this.btnKur.TabIndex = 3;
+            this.btnKur.Text = "ALARMI KUR";
+            this.btnKur.UseVisualStyleBackColor = true;
+            this.btnKur.Click += new System.EventHandler(this.btnKur_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dtpZaman
+            // 
+            this.dtpZaman.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtpZaman.Location = new System.Drawing.Point(178, 18);
+            this.dtpZaman.Name = "dtpZaman";
+            this.dtpZaman.Size = new System.Drawing.Size(165, 20);
+            this.dtpZaman.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(438, 261);
+            this.Controls.Add(this.dtpZaman);
+            this.Controls.Add(this.btnKur);
+            this.Controls.Add(this.btnDurdur);
+            this.Controls.Add(this.lblMesaj);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMesaj;
+        private System.Windows.Forms.Button btnDurdur;
+        private System.Windows.Forms.Button btnKur;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DateTimePicker dtpZaman;
+    }
+}
+
---
+++ datetime-kullanimi3/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace datetime_kullanimi3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SoundPlayer player = new SoundPlayer();
+
+        private void btnKur_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            lblMesaj.Text = "Alarm Kuruldu.";
+            btnKur.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime dateTime = DateTime.Now;
+            DateTime alarmTime = dtpZaman.Value;
+
+            if (dateTime.Hour == alarmTime.Hour && dateTime.Minute == alarmTime.Minute && dateTime.Second == alarmTime.Second)
+            {
+                timer1.Stop();
+                lblMesaj.Text = "Alarm Çalıyor...";
+
+
+                player.SoundLocation = @"D:\Downloads\Music\alarm.wav";
+                player.PlayLooping();
+            }
+        }
+
+        private void btnDurdur_Click(object sender, EventArgs e)
+        {
+            player.Stop();
+            lblMesaj.Text = "Alarm Durduruldu.";
+            btnDurdur.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblMesaj.Text = "...";
+        }
+    }   
+}
